a91c961822a274e7dd91d844ab84fc5a7f5386a1
Aadarsha Shrestha <aadarsha@tutanota.com>
2017-03-10 18:12:13 +0000

    Scrapper for newsapi.org

---
+++ scrapper/scrapper-newsapi.org.py
@@ -0,0 +1,34 @@
+##########
+#
+# scrapper.py
+# By Aadarsha Shrestha (aadarsha.shrestha.nepal@gmail.com)
+#
+# The scrapper can scrape news headlines from multiple news sources.
+# Powered by newsapi.org
+#
+# Date: 05-03-2017
+# Last Modified Date: 05-07-2017
+#
+##########
+
+#!/usr/bin/python3.5
+import json
+from urllib.request import urlopen
+
+# Specify URL appended with source and API key
+# Source in the format of newsapi.org
+# Modify this section later for user selectable sources
+source = input("Please enter headline source: ")
+url = 'https://newsapi.org/v1/articles?apiKey=30a1f0a5d59b4267a08eadae0ee64508&sortBy=top&source=' + source
+
+# Get the JSON dataset
+response = urlopen(url)
+
+# Convert bytes to string, then string to dict
+string = response.read().decode('utf-8')
+json_obj = json.loads(string)
+
+# Print the title from the json_obj
+# Later, redirect to storage
+for single_news in json_obj['articles']:
+	print(single_news['title'])
