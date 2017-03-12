338a9b0dfcd4c23309f4bd9524794a873747791b
Kuldeep Singh <kuldeeps48@gmail.com>
2017-03-12 12:11:00 +0530

    Ignore Data Folder to remove merge conflicts. Added Analyze progress bar

--- ui/progress.py
+++ ui/progress.py
@@ -1,5 +1,6 @@
 import sys, time
 from PyQt4 import QtGui, QtCore
+from PyQt4.Qt import *
 
 DEFAULT_STYLE = """QProgressBar{border: 2px solid white;
                                 border-radius: 5px;
@@ -19,32 +20,40 @@ class Window(QtGui.QMainWindow):
 
         self.setStyleSheet(DEFAULT_STYLE)
         # self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-        self.home()
+        self.createItems()
 
-    def home(self):
+    def createItems(self):
         self.progress = QtGui.QProgressBar(self)
         self.progress.setGeometry(130, 225, 600, 50)
+
+        # QLabel
+
 
         self.show()
 
     def download(self):
-        self.completed = 0
+        self.extractProgress = 0
+        while self.extractProgress < 100:
+            self.extractProgress += 0.0001
+            self.progress.setValue(self.extractProgress)
 
-        while self.completed < 100:
-            self.completed += 0.00009
-            self.progress.setValue(self.completed)
+        time.sleep(0.4)
+
+        self.analyseProgress = 0
+        while self.analyseProgress < 100:
+            self.analyseProgress += 0.00003
+            self.progress.setValue(self.analyseProgress)
 
         time.sleep(0.4)
         self.close()
+
 
 def showProgress():
     app = QtGui.QApplication(sys.argv)
     GUI = Window()
     GUI.setWindowOpacity(0.87)
     GUI.download()
+    app.processEvents()
 
-
-
-
-
-showProgress()
+if __name__ == '__main__':
+    showProgress()
