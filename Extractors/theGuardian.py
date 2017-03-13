f530631fbc10aede95143ee9868c26cf3df2001e
aadarsh977 <aadarsha@tutanota.com>
2017-03-14 00:55:28 +0530

    Make 'theGuardian.py' working again as per new template

--- Extractors/theGuardian.py
+++ Extractors/theGuardian.py
@@ -1,60 +1,55 @@
 ##########
 #
-# scrapper.py
-# By Aadarsha Shrestha (aadarsha.shrestha.nepal@gmail.com)
+# theGuardian.py
+# By Aadarsha Shrestha (aadarsha.shrestha.nepal@gmail.com, aadarsha@tutanota.com)
 #
 # Returns the headlines from The Guardian of the current day
 # API: http://open-platform.theguardian.com
+# 
+# NOTE:
+#  - url_formatter(), extractor() are to be changed according to need
+#  - Do not change the modules: get_json(), scrapper()
+# 
 # Restrictions:	
 #		Up to 12 calls per second
 #		Up to 5,000 calls per day
 #		Access to article text
-# Date: 10-03-2017
-# Last Modified Date: 10-03-2017
+#
+# Date: 11-03-2017
 #
 ##########
 
-import datetime, os, time, requests, json
-
-def guardianScrapper():
-    today = str(datetime.date.today())
-
-    directory = "./data/theGuardian/" + today
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    file = directory + "/" + today + ".txt"
+#!/usr/bin/python3
+import datetime, os, time, requests
 
 
-    # Formats the Request
-    def guardian_url_formatter():
-        cur_date = time.strftime("%Y-%m-%d")
-        api_key = '77ed6500-5bea-4fbc-9709-27b6fc6d2d60'
-        use_date = 'published'
-        url = 'http://content.guardianapis.com/search?from-date=' + cur_date + '&to-date=' + cur_date + '&api-key=' + api_key + '&use-date=' + use_date
-        return url
+source_code = "theGuardian"
 
 
-    # Fetches JSON data and parses it
-    def get_json(url):
-        # Make request
-        response = requests.get(url)
+# Formats the Request
+def url_formatter():
+    cur_date = time.strftime("%Y-%m-%d")
+    api_key = '77ed6500-5bea-4fbc-9709-27b6fc6d2d60'
+    use_date = 'published'
+    url = 'http://content.guardianapis.com/search?from-date=' + cur_date + '&to-date=' + cur_date + '&api-key=' + api_key + '&use-date=' + use_date
+    return url
 
-        json_data = response.json()
-        return json_data
+
+# Fetches JSON data and parses it
+# Returns JSON object
+def get_json(url):
+    response = requests.get(url)
+    json_data = response.json()
+    return json_data
 
 
-    ###########
-
-    url = guardian_url_formatter()
-
-    # Fetch JSON data and compute the total number of news articles
-    json_data = get_json(url)
+# Headline extractor for The Guardian
+# Returns headlines list
+def extractor(url, json_data):
     total = json_data['response']['total']
     page_no = 1
 
     headlines = []
-    counter = 0
 
     # Return news headlines
     while True:
@@ -63,13 +58,35 @@ def guardianScrapper():
 
         for single_news in json_data['response']['results']:
             headlines.append(single_news['webTitle'])
-            counter += 1
 
         total -= 200
         page_no += 1
         if total <= 0:
             break
+        
+    return headlines
 
+
+# Module to be called from extractorRunner.py
+# Returns file populated with news headlines
+def scrapper():
+    url = url_formatter()
+
+    # Fetch JSON data and compute the total number of news articles
+    json_data = get_json(url)
+    
+    headlines = extractor(url, json_data)
+    
+    # Compute file path
+    today = str(datetime.date.today())
+    
+    directory = "./data/" + source_code + "/" + today
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    file = directory + "/" + today + ".txt"
+    
+    # Write in file
     with open(file, "w") as tf:
         for headline in headlines:
             tf.write(headline + "\n")
