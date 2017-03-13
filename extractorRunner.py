ed8ced3d31fae9c6add80a52e860c10a81724842
aadarsh977 <aadarsha@tutanota.com>
2017-03-14 01:15:55 +0530

    Remove the modifications due to conflict

--- extractorRunner.py
+++ extractorRunner.py
@@ -8,13 +8,8 @@ from Extractors.theGuardian import scrapper as guardianScrapper
 from Extractors.nyTimes import scrapper as nyTimesScrapper
 
 
-<<<<<<< HEAD
 source_functions = {"google news":googleScrapper(), "reddit news":redditScrapper(), "reddit world news":redditWorldScrapper(),
                     "guardian":guardianScrapper(), "nyTimes":nyTimesScrapper()}
-=======
-source_functions = {"google news":googleScrapper, "reddit news":redditScrapper, "reddit world news":redditWorldScrapper,
-                    "guardian":guardianScrapper}
->>>>>>> db2a1cafd65296a50e468c7840f2e7fdd9b81922
 
 def runScrapper(source):  # Button press function
     if source in source_functions:
