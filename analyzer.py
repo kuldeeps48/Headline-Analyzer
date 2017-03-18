bef51cd426f62674ff307d1b0560fdeeba63e284
Kuldeep Singh <kuldeeps48@gmail.com>
2017-03-18 22:05:32 +0530

    Remove special chars, run sentiwordnet before additional dictionary help -> Better analyzer accuracy

--- analyzer.py
+++ analyzer.py
@@ -6,13 +6,9 @@ from nltk.stem import WordNetLemmatizer
 from additional import Dict # use the dictionary
 
 """ TODO:
-1) Work on argument 3 of sentiwordnet
-3) improve tokens, [WP, POS, PRP$, MD]
 2) imporove lemmatiztion
 4) imporve tagging
 5) add quote detection
-6) use additional dictionary
-WP - WH-pronoun
 """
 
 
@@ -26,12 +22,21 @@ def pos_to_senti_tag(tag_from_POS):
     if tag_from_POS in ("RB", "RBR", "RBS"):
         return "r"  # adverb
 
-    return "unknown"
-
 
 def ignore_word(tag):
     neutral_tags = ["CD", "IN", "(", ")", ",", "--", ".", ":", "LS", "''","TO"]
     return tag in neutral_tags
+
+
+def cleanHeadlineString(headline):
+    headline = headline.replace("'s", "")
+    headline = headline.replace(":", "")
+    headline = headline.replace("-", " ")
+    headline = headline.replace("'", "")
+    headline = headline.replace(",", "")
+    headline = headline.replace("?", "")
+    headline = headline.replace(";", "")
+    return headline.lower()
 
 
 def analyze(file):
@@ -40,10 +45,16 @@ def analyze(file):
             pass
         with open('data\output1.txt', "a") as f: # open file once to append data for each headline
             for headline in data:
+                if len(headline) > 120: # Skip Huge Headlines
+                    continue
+
                 print(headline)
 
+                # remove special characters & make it lower case
+                headline_to_analyze = cleanHeadlineString(headline)
+
                 # Convert headline sentence to a list of words
-                words = word_tokenize(headline)
+                words = word_tokenize(headline_to_analyze)
 
                 # Tag each word in list with POS tagger
                 tagged_words = nltk.pos_tag(words)
@@ -53,19 +64,10 @@ def analyze(file):
 
                 pos_score = 0
                 neg_score = 0
-                obj_score = 0
+                # obj_score = 0
 
                 for i in tagged_words:
                     senti_tag = pos_tag = i[1]
-
-                    # modifier scores before analysis since sentiwordnet fails to recognize them properly
-                    if str.lower(i[0]) in Dict:
-                        score = float(Dict[str.lower(i[0])])
-                        if score < 0:
-                            neg_score += score
-                        else:
-                            pos_score += score
-                        continue
 
                     # Check if word creates no opinion, else tag them properly
                     # nltk.help.upenn_tagset()
@@ -74,15 +76,12 @@ def analyze(file):
                     else:
                         senti_tag = pos_to_senti_tag(i[1])
 
-                    if senti_tag == "unknown":
-                        continue
-
                     # Lemmatize the word according to its tag
                     # make argument 2 accurate
                     try:
                         lem_word = w_lem.lemmatize(i[0], senti_tag)
                     except:
-                        continue
+                        lem_word = i[0]
 
                     # print(lem_word," ", senti_tag)
                     # Get the numerical equivalent sentiwordnet score
@@ -90,21 +89,27 @@ def analyze(file):
                     try:
                         breakdown = swn.senti_synset(lem_word + "." + senti_tag + "." + "01")
                         # print(breakdown)
-                    except nltk.corpus.reader.wordnet.WordNetError:
-                        # print(lem_word," ",senti_tag)
-                        continue
                     except:
-                        continue
+                        # modifier scores before analysis since sentiwordnet fails to recognize them properly
+                        if i[0] in Dict:
+                            score = float(Dict[i[0]])
+                            if score < 0:
+                                neg_score += score
+                            else:
+                                pos_score += score
 
                     pos_score += breakdown.pos_score()
                     neg_score += breakdown.neg_score()
-                    obj_score += breakdown.obj_score()
+                    # obj_score += breakdown.obj_score()
 
-                print("P=", pos_score, end="")
-                print("    |    N=", neg_score)
+                # print("P=", pos_score, end="")
+                # print("    |    N=", neg_score)
+                if neg_score > 0:
+                    neg_score = -neg_score
 
-                f.write(headline)
-                f.write("  >P="+str(pos_score)+"  >N="+str(neg_score)+"\n")
+                print("Score = ", (pos_score + neg_score))
+                f.write(headline + ">> " + str(pos_score + neg_score))
+                # f.write("  >P="+str(pos_score)+"  >N="+str(neg_score)+"\n")
 
                 print("\n")
 
