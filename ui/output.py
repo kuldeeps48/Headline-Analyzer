b2309f55cb05b38c2a8dc1459e37e90cec043d0d
Kuldeep Singh <kuldeeps48@gmail.com>
2017-03-21 22:20:44 +0530

    Added weekly analysis. Output is now complete \m/

--- ui/output.py
+++ ui/output.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import images.outputResources # Image file
+import images.outputResources  # Image file
 from PyQt4 import QtCore, QtGui
 from pyqtgraph import PlotWidget
 import pyqtgraph as pg
 from wordcloud import WordCloud
-import sys
+import sys, os
 from pathlib import Path
+from datetime import date, timedelta
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -16,25 +17,28 @@ except AttributeError:
 
 try:
     _encoding = QtGui.QApplication.UnicodeUTF8
+
+
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig, _encoding)
 except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
-
 displayingfile = ""
-displayingHeadlines = ["A","B","C","D"]
+displayingHeadlines = ["A", "B", "C", "D"]
 displayingScores = [0.0, 0.0, 0.0, 0.0]
 lineNumber = 0
+outputDate = date.today()
+
 
 def drawWordCloud():
     # Read the whole text.
     text = open(displayingfile).read()
     # Generate a word cloud image
-    wordcloud = WordCloud(width=560,height=321).generate(text)
+    wordcloud = WordCloud(width=560, height=321).generate(text)
     wordcloud.to_file("./data/wc.png")
-    #wordcloud.to_file("../data/wc.png") #testing
+    # wordcloud.to_file("../data/wc.png") #testing
 
 
 def makeGraph(graphView):
@@ -46,29 +50,54 @@ def makeGraph(graphView):
             if line.startswith(">>"):
                 num = float(line[3:].rstrip("\n"))
                 y_axis.append(num)
-    x_axis = list(range(1,len(y_axis) + 1))
+    x_axis = list(range(1, len(y_axis) + 1))
     graphView.plot(x_axis, y_axis, pen=(200, 200, 200), symbolBrush=(255, 0, 0), symbolPen='b')
 
 
 class Ui_Dialog(object):
+    def oneWeekAnalysis(self):  # Graph plot for 1 week scores
+        pg.setConfigOptions(antialias=True)
+        global displayingfile
+        global outputDate
+        today = outputDate  # Get output date, compute 1 week from that date
+        y_axis = []
+        for i in range(0, 7):
+            temp = displayingfile[:-31]
+            weekDate = today - timedelta(days=i)
+            temp += str(weekDate) + "\\" + str(weekDate) + "scores.txt"
+            try:
+                with open(temp, "r") as f:
+                    for line in f:
+                        if line.startswith(">>"):
+                            num = float(line[3:].rstrip("\n"))
+                            y_axis.append(num)
+            except FileNotFoundError:  # If data does not exists, ignore
+                continue
+
+        x_axis = list(range(1, len(y_axis) + 1))
+        self.Graph.plotItem.clear()  # reset graph
+        self.Graph.plot(x_axis, y_axis, pen=(200, 200, 200), symbolBrush=(255, 0, 0), symbolPen='b')
+
     def dateSelected(self):
         x = self.calendarWidget.selectedDate()
         day = x.day()
         month = x.month()
         year = x.year()
-        if day < 10: # since we get 1,2,3,4...instead of 01,02,03...
+        if day < 10:  # since we get 1,2,3,4...instead of 01,02,03...
             day = "0" + str(day)
         if month < 10:
             month = "0" + str(month)
 
         selected_date = str(year) + "-" + str(month) + "-" + str(day)
+        global outputDate
+        outputDate = date(int(year), int(month), int(day))  # Set selected date as output data
         global displayingfile
-        displayingfile = displayingfile[:-31] # remove previous dates from file location name
+        displayingfile = displayingfile[:-31]  # remove previous dates from file location name
         displayingfile += selected_date + "\\" + selected_date + "scores.txt"
 
         my_file = Path(displayingfile)
-        if my_file.is_file(): # if file exists for selected date
-            self.Graph.plotItem.clear() #reset graph
+        if my_file.is_file():  # if file exists for selected date
+            self.Graph.plotItem.clear()  # reset graph
             makeGraph(self.Graph)
 
             drawWordCloud()
@@ -79,20 +108,18 @@ class Ui_Dialog(object):
             lineNumber = 0
             self.nextFourHeadlines()
 
-
-
     def nextFourHeadlines(self):
         count = 0
         global lineNumber
-        with open(displayingfile,"r") as f:
-            data = f.readlines()[lineNumber:lineNumber+9]
+        with open(displayingfile, "r") as f:
+            data = f.readlines()[lineNumber:lineNumber + 9]
             data1 = data[:8:2]  # line 1-8 in increment of 2 since 2nd line has scores
             data2 = data[1:9:2]  # line 2-9 in increment of 2 since 1st line has headlines
-            for i,line in enumerate(data1):  # Remove \n from headlines
+            for i, line in enumerate(data1):  # Remove \n from headlines
                 temp = line[:-1]
                 data1[i] = temp
 
-            for i,line in enumerate(data2):  # Remove >> and \n from scores
+            for i, line in enumerate(data2):  # Remove >> and \n from scores
                 temp = line[3:-1]
                 data2[i] = temp
 
@@ -115,16 +142,15 @@ class Ui_Dialog(object):
                          color:#ffffff;\"><b>" + displayingHeadlines[3] + "</b></span></p></body></html>", None))
 
                 self.Value1.setText(_translate("Dialog", "<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; \
-                         color:#ffffff;\"><b>" + displayingScores[0] + "</b></span></p></body></html>",None))
+                         color:#ffffff;\"><b>" + displayingScores[0] + "</b></span></p></body></html>", None))
                 self.Value2.setText(_translate("Dialog", "<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; \
                          color:#ffffff;\"><b>" + displayingScores[1] + "</b></span></p></body></html>", None))
-                self.Value3.setText(_translate("Dialog","<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; \
-                         color:#ffffff;\"><b>" + displayingScores[2] + "</b></span></p></body></html>",None))
+                self.Value3.setText(_translate("Dialog", "<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; \
+                         color:#ffffff;\"><b>" + displayingScores[2] + "</b></span></p></body></html>", None))
                 self.Value4.setText(_translate("Dialog", "<html><head/><body><p align=\"center\"><span style=\" font-size:14pt;\
                          color:#ffffff;\"><b>" + displayingScores[3] + "</b></span></p></body></html>", None))
             except IndexError:
                 pass
-
 
     def setupUi(self, Dialog):
         Dialog.setObjectName(_fromUtf8("Dialog"))
@@ -136,12 +162,13 @@ class Ui_Dialog(object):
         Dialog.setSizePolicy(sizePolicy)
         Dialog.setFocusPolicy(QtCore.Qt.StrongFocus)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/icon/Project/HeadlineMining Gitlab/images/icon.ico")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/icon/Project/HeadlineMining Gitlab/images/icon.ico")),
+                       QtGui.QIcon.Normal, QtGui.QIcon.Off)
         Dialog.setWindowIcon(icon)
         Dialog.setAutoFillBackground(False)
         Dialog.setStyleSheet(_fromUtf8("background-image: url(:/bavkground/images/woodback.jpg);"))
 
-        #Graph
+        # Graph
         pg.setConfigOption('background', 'w')
         pg.setConfigOption('foreground', 'k')
         pg.setConfigOption('leftButtonPan', False)
@@ -156,12 +183,12 @@ class Ui_Dialog(object):
         self.wordCloud.setObjectName(_fromUtf8("wordCloud"))
         drawWordCloud()
         self.wordCloud.setPixmap(QtGui.QPixmap("./data/wc.png"))
-        #self.wordCloud.setPixmap(QtGui.QPixmap("../data/wc.png")) #testing
+        # self.wordCloud.setPixmap(QtGui.QPixmap("../data/wc.png")) #testing
 
 
 
 
-        #Calender
+        # Calender
         self.calendarWidget = QtGui.QCalendarWidget(Dialog)
         self.calendarWidget.setGeometry(QtCore.QRect(830, 430, 361, 251))
         self.calendarWidget.setMinimumDate(QtCore.QDate(2017, 1, 2))
@@ -169,36 +196,35 @@ class Ui_Dialog(object):
         self.calendarWidget.setObjectName(_fromUtf8("calendarWidget"))
         self.calendarWidget.clicked.connect(self.dateSelected)
 
-
-        #Calender Label
+        # Calender Label
         self.CalenderLabel = QtGui.QLabel(Dialog)
         self.CalenderLabel.setGeometry(QtCore.QRect(770, 370, 491, 51))
         self.CalenderLabel.setAutoFillBackground(False)
         self.CalenderLabel.setStyleSheet(_fromUtf8("background:transparent;"))
         self.CalenderLabel.setFrameShadow(QtGui.QFrame.Plain)
-        self.CalenderLabel.setText(_fromUtf8("<html><head/><body><p align=\"center\"><span style=\" font-size:12pt; font-weight:600; color:#fff2af;\"><u>Select a date from calender below to plot that day\'s graph</u></span></p></body></html>"))
+        self.CalenderLabel.setText(_fromUtf8(
+            "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt; font-weight:600; color:#fff2af;\"><u>Select a date from calender below to plot that day\'s graph</u></span></p></body></html>"))
         self.CalenderLabel.setTextFormat(QtCore.Qt.RichText)
         self.CalenderLabel.setAlignment(QtCore.Qt.AlignCenter)
         self.CalenderLabel.setWordWrap(True)
         self.CalenderLabel.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
         self.CalenderLabel.setObjectName(_fromUtf8("CalenderLabel"))
 
-
-        #Headline1
+        # Headline1
         self.Headline1 = QtGui.QLabel(Dialog)
         self.Headline1.setGeometry(QtCore.QRect(40, 392, 521, 51))
         self.Headline1.setStyleSheet(_fromUtf8("background:transparent;"))
         self.Headline1.setTextFormat(QtCore.Qt.RichText)
-        self.Headline1.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignCenter|QtCore.Qt.AlignHCenter)
+        self.Headline1.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignCenter | QtCore.Qt.AlignHCenter)
         self.Headline1.setWordWrap(True)
         self.Headline1.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
         self.Headline1.setObjectName(_fromUtf8("Headline1"))
 
-        #Nextbutton
+        # Nextbutton
         self.pushButton = QtGui.QPushButton(Dialog)
         self.pushButton.setGeometry(QtCore.QRect(670, 490, 81, 81))
         self.pushButton.setStyleSheet(_fromUtf8("background-image: url(:/labelBackground/images/nextButton.png);\n"
-"border:none;"))
+                                                "border:none;"))
         self.pushButton.setAutoDefault(True)
         self.pushButton.isCheckable()
         self.pushButton.setDefault(False)
@@ -206,8 +232,7 @@ class Ui_Dialog(object):
         self.pushButton.setObjectName(_fromUtf8("pushButton"))
         self.pushButton.clicked.connect(self.nextFourHeadlines)
 
-
-        #WeeklyButton
+        # WeeklyButton
         self.weekButton = QtGui.QPushButton(Dialog)
         self.weekButton.setGeometry(QtCore.QRect(1210, 490, 130, 81))
         self.weekButton.setStyleSheet(_fromUtf8("color:white; font-size:14pt; font-weight:600; background:grey; \
@@ -218,9 +243,9 @@ class Ui_Dialog(object):
         self.weekButton.isCheckable()
         self.weekButton.setText("One Week \nAnalysis")
         self.weekButton.setObjectName(_fromUtf8("weekButton"))
-        #self.weekButton.clicked.connect(self.nextFourHeadlines)
+        self.weekButton.clicked.connect(self.oneWeekAnalysis)
 
-        #Value1
+        # Value1
         self.Value1 = QtGui.QLabel(Dialog)
         self.Value1.setGeometry(QtCore.QRect(580, 392, 71, 51))
         self.Value1.setStyleSheet(_fromUtf8("background:transparent;"))
@@ -229,40 +254,33 @@ class Ui_Dialog(object):
         self.Value1.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
         self.Value1.setObjectName(_fromUtf8("Value1"))
 
-
-
-
-
-        #Headline2
+        # Headline2
         self.Headline2 = QtGui.QLabel(Dialog)
         self.Headline2.setGeometry(QtCore.QRect(40, 472, 521, 51))
         self.Headline2.setStyleSheet(_fromUtf8("background:transparent;"))
         self.Headline2.setTextFormat(QtCore.Qt.RichText)
-        self.Headline2.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignCenter|QtCore.Qt.AlignHCenter)
+        self.Headline2.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignCenter | QtCore.Qt.AlignHCenter)
         self.Headline2.setWordWrap(True)
         self.Headline2.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
         self.Headline2.setObjectName(_fromUtf8("Headline2"))
-
 
         self.Headline3 = QtGui.QLabel(Dialog)
         self.Headline3.setGeometry(QtCore.QRect(40, 552, 521, 51))
         self.Headline3.setStyleSheet(_fromUtf8("background:transparent;"))
         self.Headline3.setTextFormat(QtCore.Qt.RichText)
-        self.Headline3.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignCenter|QtCore.Qt.AlignHCenter)
+        self.Headline3.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignCenter | QtCore.Qt.AlignHCenter)
         self.Headline3.setWordWrap(True)
         self.Headline3.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
         self.Headline3.setObjectName(_fromUtf8("Headline3"))
-
 
         self.Headline4 = QtGui.QLabel(Dialog)
         self.Headline4.setGeometry(QtCore.QRect(40, 632, 521, 51))
         self.Headline4.setStyleSheet(_fromUtf8("background:transparent;"))
         self.Headline4.setTextFormat(QtCore.Qt.RichText)
-        self.Headline4.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignCenter|QtCore.Qt.AlignHCenter)
+        self.Headline4.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignCenter | QtCore.Qt.AlignHCenter)
         self.Headline4.setWordWrap(True)
         self.Headline4.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
         self.Headline4.setObjectName(_fromUtf8("Headline4"))
-
 
         self.Value4 = QtGui.QLabel(Dialog)
         self.Value4.setGeometry(QtCore.QRect(580, 632, 71, 51))
@@ -291,20 +309,37 @@ class Ui_Dialog(object):
 
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(_translate("Dialog", "Analysis Of Extracted Headlines", None))
-        self.Headline1.setText(_translate("Dialog", "<html><head/><body><p align=\"justify\"><span style=\" font-size:10pt; color:#ffffff;\">Headline 1</span></p></body></html>", None))
-        self.Value1.setText(_translate("Dialog", "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt; color:#ffffff;\">0.0</span></p></body></html>", None))
-        self.Headline2.setText(_translate("Dialog", "<html><head/><body><p align=\"justify\"><span style=\" font-size:10pt; color:#ffffff;\">Headline 1</span></p></body></html>", None))
-        self.Headline3.setText(_translate("Dialog", "<html><head/><body><p align=\"justify\"><span style=\" font-size:10pt; color:#ffffff;\">Headline 1</span></p></body></html>", None))
-        self.Headline4.setText(_translate("Dialog", "<html><head/><body><p align=\"justify\"><span style=\" font-size:10pt; color:#ffffff;\">Headline 1</span></p></body></html>", None))
-        self.Value4.setText(_translate("Dialog", "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt; color:#ffffff;\">0.0</span></p></body></html>", None))
-        self.Value3.setText(_translate("Dialog", "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt; color:#ffffff;\">0.0</span></p></body></html>", None))
-        self.Value2.setText(_translate("Dialog", "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt; color:#ffffff;\">0.0</span></p></body></html>", None))
+        self.Headline1.setText(_translate("Dialog",
+                                          "<html><head/><body><p align=\"justify\"><span style=\" font-size:10pt; color:#ffffff;\">Headline 1</span></p></body></html>",
+                                          None))
+        self.Value1.setText(_translate("Dialog",
+                                       "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt; color:#ffffff;\">0.0</span></p></body></html>",
+                                       None))
+        self.Headline2.setText(_translate("Dialog",
+                                          "<html><head/><body><p align=\"justify\"><span style=\" font-size:10pt; color:#ffffff;\">Headline 1</span></p></body></html>",
+                                          None))
+        self.Headline3.setText(_translate("Dialog",
+                                          "<html><head/><body><p align=\"justify\"><span style=\" font-size:10pt; color:#ffffff;\">Headline 1</span></p></body></html>",
+                                          None))
+        self.Headline4.setText(_translate("Dialog",
+                                          "<html><head/><body><p align=\"justify\"><span style=\" font-size:10pt; color:#ffffff;\">Headline 1</span></p></body></html>",
+                                          None))
+        self.Value4.setText(_translate("Dialog",
+                                       "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt; color:#ffffff;\">0.0</span></p></body></html>",
+                                       None))
+        self.Value3.setText(_translate("Dialog",
+                                       "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt; color:#ffffff;\">0.0</span></p></body></html>",
+                                       None))
+        self.Value2.setText(_translate("Dialog",
+                                       "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt; color:#ffffff;\">0.0</span></p></body></html>",
+                                       None))
 
 
-def showOutput(): #testing: add parameter
+def showOutput():  # testing: add parameter
+    # os.chdir("../") Testing
     global displayingfile
     displayingfile = sys.argv[1]  # Take scores file as command line argument #testing
-    #displayingfile = file # testing
+    # displayingfile = file  # testing
     app = QtGui.QApplication(sys.argv)
     Dialog = QtGui.QDialog()
     ui = Ui_Dialog()
@@ -314,8 +349,8 @@ def showOutput(): #testing: add parameter
     app.processEvents()
     sys.exit(app.exec_())
 
+
 if __name__ == "__main__":
     showOutput()
-    #for testing purpose
-    #showOutput(r'C:\Users\Kuldeep\Desktop\Project\HeadlineMining Gitlab\data\nyTimes\2017-03-20\2017-03-20scores.txt')
-
+    # for testing purpose
+    # showOutput(r'C:\Users\Kuldeep\Desktop\Project\HeadlineMining Gitlab\data\nyTimes\2017-03-20\2017-03-20scores.txt')
