d8b9faea43ba0bdf26d9699157cdca8640d0c6ca
aadarsh977 <aadarsha@tutanota.com>
2017-03-24 19:12:14 +0530

    Add The Hindu scrapper

---
+++ Extractors/theHindu.py
@@ -0,0 +1,61 @@
+##########
+#
+# theHindu.py
+# By Aadarsha Shrestha (aadarsha.shrestha.nepal@gmail.com, aadarsha@tutanota.com)
+#
+# Returns the current day's headlines from The Hindu
+# API: (RSS) http://www.thehindu.com/news/?service=rss
+#
+# NOTE:
+#  - Update URLs only
+#
+#
+# Date: 24-03-2017
+#
+##########
+
+# !/usr/bin/python3
+import datetime, os, feedparser
+SOURCE_CODE = "theHindu"
+
+
+# Headline extractor
+# Returns headlines list
+def extractor(headlines):
+    # URL for RSS feed
+    base_url = 'http://www.thehindu.com/news/?service=rss'
+
+    feed = feedparser.parse(base_url)
+
+    # Append headline to list
+    for single_news in feed['entries']:
+        headlines.append(single_news["title"])
+        #headlines.append(unidecode.unidecode(single_news["title"]))
+
+
+# Module to be called from extractorRunner.py
+# Returns file populated with news headlines
+def scrapper():
+    # Initialize headlines list
+    headlines = []
+
+    extractor(headlines)
+
+    # Compute file path
+    today = str(datetime.date.today())
+
+    directory = "./data/" + SOURCE_CODE + '/' + today
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    file = directory + "/" + today + ".txt"
+
+    # Write in file
+    with open(file, "w") as tf:
+        for headline in headlines:
+            try:
+                tf.write(headline + "\n")
+            except:
+                pass
+
+    return file
